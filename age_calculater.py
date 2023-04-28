e49e7a07c137109ec6e0d0bd272e9c4e5e8e8b40
Ashish Verma <100463212+ashishverma5218@users.noreply.github.com>
2023-04-28 21:23:44 +0530

    Delete age_calculater.py

--- age_calculater.py
+++
@@ -1,6 +0,0 @@
-age = input("What is your current age?\n")
-left_age = 100 - int(age)
-Days = left_age*365
-Weeks = left_age*52
-Months = left_age*12
-print(f'You have {Days} Days , {Weeks} Weeks , and {Months} Months left. If you live 100 years')
